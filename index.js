75065d76bdc3394a0f36457fd03f8470a8776134
Syed Safder Qasim <ssqasim@csquareonline.com>
2018-12-27 09:59:03 +0500

    remove docker

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello World Team!'));
+app.get('/', (req, res) => res.send('Hello World CC Testing!'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
